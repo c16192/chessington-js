18a99fac0dd6bb8487512fbe2ac12ce58eb1e0a4
c16192 <ishidashu@gmail.com>
2018-06-27 12:21:02 +0100

    Queen

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -12,21 +12,20 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board): Square[] {
-            const currentSquare = board.findPiece(this);
-            let movePatterns: Move[] = [];
-            for(let i = 1; i < GameSettings.BOARD_SIZE; i++){
-                movePatterns.push(new Move(i,0));
-                movePatterns.push(new Move(-i,0));
-                movePatterns.push(new Move(0,i));
-                movePatterns.push(new Move(0,-i));
-                movePatterns.push(new Move(i,i));
-                movePatterns.push(new Move(i,-i));
-                movePatterns.push(new Move(-i,i));
-                movePatterns.push(new Move(-i,-i));
-            }
-            const moves = Moves.getMoves(board, this, currentSquare, movePatterns);
-            console.log(moves)
-            return moves;
+        const currentSquare = board.findPiece(this);
+        let movePatterns: Move[] = [];
+        for(let i = 1; i < GameSettings.BOARD_SIZE; i++){
+            movePatterns.push(new Move(i,0));
+            movePatterns.push(new Move(-i,0));
+            movePatterns.push(new Move(0,i));
+            movePatterns.push(new Move(0,-i));
+            movePatterns.push(new Move(i,i));
+            movePatterns.push(new Move(i,-i));
+            movePatterns.push(new Move(-i,i));
+            movePatterns.push(new Move(-i,-i));
         }
+        const moves = Moves.getMoves(board, this, currentSquare, movePatterns);
+        console.log(moves)
+        return moves;
     }
 }
