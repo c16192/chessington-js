a6c010049eb609906f69c9b3a628516dfa196f1d
c16192 <ishidashu@gmail.com>
2018-06-27 12:20:33 +0100

    Queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -9,13 +9,30 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 import Piece from './piece';
+import Move from "../move";
+import GameSettings from "../gameSettings";
+import Moves from "../moves";
 var Queen = /** @class */ (function (_super) {
     __extends(Queen, _super);
     function Queen(player) {
         return _super.call(this, player) || this;
     }
     Queen.prototype.getAvailableMoves = function (board) {
-        return new Array(0);
+        var currentSquare = board.findPiece(this);
+        var movePatterns = [];
+        for (var i = 1; i < GameSettings.BOARD_SIZE; i++) {
+            movePatterns.push(new Move(i, 0));
+            movePatterns.push(new Move(-i, 0));
+            movePatterns.push(new Move(0, i));
+            movePatterns.push(new Move(0, -i));
+            movePatterns.push(new Move(i, i));
+            movePatterns.push(new Move(i, -i));
+            movePatterns.push(new Move(-i, i));
+            movePatterns.push(new Move(-i, -i));
+        }
+        var moves = Moves.getMoves(board, this, currentSquare, movePatterns);
+        console.log(moves);
+        return moves;
     };
     return Queen;
 }(Piece));
--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,13 +1,32 @@
 import Piece from './piece';
 import Board from "../board";
 import {PlayerType} from "../player";
+import Square from "../square";
+import Move from "../move";
+import GameSettings from "../gameSettings";
+import Moves from "../moves";
 
 export default class Queen extends Piece {
     constructor(player: PlayerType) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+            const currentSquare = board.findPiece(this);
+            let movePatterns: Move[] = [];
+            for(let i = 1; i < GameSettings.BOARD_SIZE; i++){
+                movePatterns.push(new Move(i,0));
+                movePatterns.push(new Move(-i,0));
+                movePatterns.push(new Move(0,i));
+                movePatterns.push(new Move(0,-i));
+                movePatterns.push(new Move(i,i));
+                movePatterns.push(new Move(i,-i));
+                movePatterns.push(new Move(-i,i));
+                movePatterns.push(new Move(-i,-i));
+            }
+            const moves = Moves.getMoves(board, this, currentSquare, movePatterns);
+            console.log(moves)
+            return moves;
+        }
     }
 }
